76eebeee231768c941453aab29a5cd160fa727d7
Callan Morrison <callan.l.morrison@gmail.com>
2017-04-23 21:30:48 -0500

    strings

---
+++ variables/name_cases.py
@@ -0,0 +1,19 @@
+# Personal Message
+name = "Eric"
+print("Hello, " + name + "! Would you like to learn some Python today?")
+
+# Name Cases
+print("Expect title format", name.title())
+print("Expect lowercase format", name.lower())
+print("Expect uppercase format", name.upper())
+
+# Famous Quote
+print('Liz Lemon once said "blergh."')
+
+# Famous Quote 2
+famous_person = "liz lemon"
+print(famous_person.title() + 'once said "blergh."')
+
+# Stripping Names
+name = " eric "
+print(name + "\n\t" + name.strip().title())
