26aff8f68a51c2bf1c0996dc68e0aaba522e7844
Callan Morrison <callan.l.morrison@gmail.com>
2017-05-07 23:05:55 -0500

    loops

--- loops/loops.py
+++ loops/loops.py
@@ -2,3 +2,19 @@ magicians = ["alice", "david", "carolina"]
 
 for magician in magicians:
     print(magician.title())
+
+for value in range(1, 5):
+    print(value)
+
+even_numers = list(range(2, 11, 2))
+
+for value in even_numers:
+    print(value)
+
+squares = []
+
+for value in range(1, 11):
+    square = value**2
+    squares.append(square)
+
+print(squares)
