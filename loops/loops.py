29306debcca3d18565718df38965c211ff569d7e
Callan Morrison <callan.l.morrison@gmail.com>
2017-05-14 21:35:51 -0500

    places to visit

--- loops/loops.py
+++ loops/loops.py
@@ -18,3 +18,8 @@ for value in range(1, 11):
     squares.append(square)
 
 print(squares)
+
+places_to_visit = ["Disney World", "Harry Potter World", "New Zealand"]
+
+for place in places_to_visit:
+    print("I want to visit " + place)
