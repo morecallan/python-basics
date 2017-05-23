66f5e3a761beb932de9bdf66b5ec06c94536e879
Callan Morrison <callan.l.morrison@gmail.com>
2017-05-22 22:35:33 -0500

    tuples

---
+++ loops/tuples.py
@@ -0,0 +1,11 @@
+# Buffet (p71)
+buffet_foods = ("hambuger", "fries", "oranges", "fish", "lobster")
+
+for food in buffet_foods:
+    print(food)
+
+# buffet_foods[1] = "turd"
+
+buffet_foods = ("hambuger", "pears", "oranges", "crab", "lobster")
+for food in buffet_foods:
+    print(food)
