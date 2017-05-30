9d464c4400efb8de16e7edd07b06da59867f26fa
Callan Morrison <callan.l.morrison@gmail.com>
2017-05-29 20:57:37 -0500

    dictionaries 5

--- variables/dictionaries.py
+++ variables/dictionaries.py
@@ -1,6 +1,5 @@
 # 6.1 Person:
 megen = {'first_name': 'Megen', 'last_name': 'Mitchell', 'age': 26, 'city': 'Boone, NC'}
-
 print("Her name is " + megen['first_name'].title() + " " + megen['last_name'].title() + ". She is " + str(megen['age']) + " years old and lives in " + megen['city'] + ".")
 
 
@@ -19,6 +18,26 @@ print('Heather\'s favorite number is: ' + str(fav_nums['heather']))
 print('Marissa\'s favorite number is: ' + str(fav_nums['marissa']))
 print('Leigh\'s favorite number is: ' + str(fav_nums['leigh']))
 
+
 # 6.4 Favorite Numbers 2
 for k, v in sorted(fav_nums.items()):
     print(k.title() + '\'s favorite number is: ' + str(v))
+
+
+# 6.6 Rivers
+rivers = {
+    'nile': 'egypt',
+    'james': 'united states',
+    'yangtze': 'china'
+}
+
+for river, country in rivers.items():
+    print("The " + river.title() + " runs through " + country.title() + ".")
+
+print("This is just a list of all the rivers:  ")
+for river in sorted(rivers):
+    print(river.title())
+
+print("This is just a list of all the countries:  ")
+for country in sorted(rivers.values()):
+    print(country.title())
