0a2cf2c32d71ab1ae9cf61d9d14a5128de6d772e
Callan Morrison <callan.l.morrison@gmail.com>
2017-05-29 20:44:07 -0500

    dictionaries 2

--- variables/dictionaries.py
+++ variables/dictionaries.py
@@ -2,3 +2,19 @@
 megen = {'first_name': 'Megen', 'last_name': 'Mitchell', 'age': 26, 'city': 'Boone, NC'}
 
 print("Her name is " + megen['first_name'].title() + " " + megen['last_name'].title() + ". She is " + str(megen['age']) + " years old and lives in " + megen['city'] + ".")
+
+
+# 6.2 Favorite Numbers
+fav_nums = {
+    'megen': 291,
+    'sarah': 3,
+    'heather': 18,
+    'marissa': 21,
+    'leigh': 99
+}
+
+print('Megen\'s favorite number is: ' + str(fav_nums['megen']))
+print('Sarah\'s favorite number is: ' + str(fav_nums['sarah']))
+print('Heather\'s favorite number is: ' + str(fav_nums['heather']))
+print('Marissa\'s favorite number is: ' + str(fav_nums['marissa']))
+print('Leigh\'s favorite number is: ' + str(fav_nums['leigh']))
