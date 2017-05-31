44ced71fd191c8be3054ae5829fff062657cf3d8
Callan Morrison <callan.l.morrison@gmail.com>
2017-05-30 23:30:33 -0500

    user input 2

--- user-input.py
+++ user-input.py
@@ -1,5 +1,15 @@
 # 7.1 Rental Car
 
-desired_car = input("What car would you like to rent?")
-
+desired_car = input("What car would you like to rent?: \n")
 print("Let me see if I can find you a " + desired_car.title() + ".")
+
+
+# 7.2 Restaurant Seating
+
+num_in_party = input("How many people are in your partay?: \n")
+num_in_party = int(num_in_party)
+
+if num_in_party > 8:
+    print("You'll have to wait for a table, homies")
+else:
+    print("We'll get you seated right away.")
