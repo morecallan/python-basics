c7d123db98695c14930e13f9b32616d981d4e61a
Callan Morrison <callan.l.morrison@gmail.com>
2017-07-06 20:23:14 -0500

    functions 2

--- functions/function.py
+++ functions/function.py
@@ -1,7 +1,15 @@
-# 8.1
+# 8.1 Message
 
 def display_message():
     """ Displays message to user about user."""
     print('I\'m learnin\' \'bout functions.')
 
 display_message()
+
+
+# 8.2 Favorite Book
+def favorite_book(title):
+    """Displays user's favorite book give the book's title."""
+    print('My favorite book is ' + title.title() + '.')
+
+favorite_book("alice in wonderland")
