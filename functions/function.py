f235923ade420c6fdb42c5a4a50457f052401a32
Callan Morrison <callan.l.morrison@gmail.com>
2017-07-06 20:56:09 -0500

    functions 3

--- functions/function.py
+++ functions/function.py
@@ -13,3 +13,14 @@ def favorite_book(title):
     print('My favorite book is ' + title.title() + '.')
 
 favorite_book("alice in wonderland")
+
+
+# 8.3 T-Shirt
+
+def make_tshirt(size, text_of_message):
+    print("You should make a shirt: ")
+    print("\tSize: " + size)
+    print("\tText: " + text_of_message)
+
+make_tshirt('X-Small', 'Peace, bitch.')
+make_tshirt(text_of_message='Peace, bitch.', size='X-Small')
